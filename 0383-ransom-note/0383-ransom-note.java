411f5f5f6cbc676b3f2e82f57e6bdf3f3b2760b4
tripathivipul007 <94601771+tripathivipul007@users.noreply.github.com>
2024-06-04 23:54:43 +0530

    Time: 10 ms (58.52%), Space: 45.2 MB (14.31%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,26 +1,23 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int temp;
-        for(int i=0; i<magazine.length(); i++){
-            if(map.containsKey(magazine.charAt(i))){
-                temp = map.get(magazine.charAt(i));
-                map.put(magazine.charAt(i), temp+1);
-            }
-            else{
-            map.put(magazine.charAt(i), 1);
-            }
-        }
-        for(int i=0; i<ransomNote.length(); i++){
-            if(!map.containsKey(ransomNote.charAt(i)) || map.get(ransomNote.charAt(i)) == 0) {
+        // Initialize a frequency map for characters in magazine
+        HashMap<Character, Integer> freqMap = new HashMap<>();
 
+        // Populate the frequency map
+        for (char ch : magazine.toCharArray()) {
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        }
+
+        // Check if ransomNote can be constructed
+        for (char ch : ransomNote.toCharArray()) {
+            if (!freqMap.containsKey(ch) || freqMap.get(ch) == 0) {
                 return false;
             }
-            else{
-                temp = map.get(ransomNote.charAt(i));
-                map.put(ransomNote.charAt(i), temp-1);
-            }
+            freqMap.put(ch, freqMap.get(ch) - 1);
         }
+
         return true;
     }
 }
