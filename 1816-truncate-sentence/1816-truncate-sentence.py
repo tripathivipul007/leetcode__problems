cd5e222e7a62e8512a2cd4e33e12371a9e048306
tripathivipul007 <94601771+tripathivipul007@users.noreply.github.com>
2024-08-23 21:51:08 +0530

    Time: 34 ms (63.96%), Space: 16.5 MB (77.38%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,7 +1,4 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        
-        ar = s.split()
-        ar = ar[:k]
-        return ' '.join(ar)
+        return ' '.join(s.split()[:k])
         
