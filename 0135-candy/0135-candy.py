e1adbaeb37f55ff7b4061af14ebc9bc883c8dfea
tripathivipul007 <94601771+tripathivipul007@users.noreply.github.com>
2025-07-20 01:10:54 +0530

    Time: 11 ms (91.41%), Space: 19.9 MB (69.52%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,15 +1,23 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        candies = [1]*n
+        up = down = peak = 0
+        total = 1  # First child gets 1 candy
 
-        for i in range(1,n):
-            if ratings[i]>ratings[i-1]:
-                candies[i] = candies[i-1] +1
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:  # Going uphill
+                up += 1
+                peak = up
+                down = 0
+                total += up + 1
+            elif ratings[i] < ratings[i - 1]:  # Going downhill
+                up = 0
+                down += 1
+                total += down
+                if down > peak:
+                    total += 1  # Fix peak point if downhill is longer
+            else:
+                up = down = peak = 0
+                total += 1
 
-        for i in range(n-2,-1,-1):
-            if ratings[i]>ratings[i+1]:
-                candies[i] = max(candies[i], candies[i+1]+1)
-
-
-        return sum(candies)
+        return total
